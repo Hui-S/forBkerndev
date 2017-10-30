aede472fb852823ff0bb42f5b926eeaa2f5687f7
Hui-S <Hui__S@outlook.com>
2017-10-30 21:24:39 +0800

    From this html page: http://www.osdever.net/bkerndev/Docs/creatingmain.htm

--- build.sh
+++ build.sh
@@ -2,14 +2,29 @@
 #Author: Hui
 #Date: 2017-10-30
 
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+ObjDIR="${DIR}/obj"
+BinDIR="${DIR}/bin"
+
+echo -e "Current directory: ${DIR}\n"
+
 echo "Now assembling, compiling, and linking your kernel:"
 
-nasm -f aout -o start.o start.asm
+if [ ! -d ${ObjDIR} ]; then
+    mkdir ${ObjDIR}
+fi
+
+if [ ! -d ${BinDIR} ]; then
+    mkdir ${BinDIR}
+fi
+
+nasm -f aout -o ${ObjDIR}/start.o start.asm
 
 # Remember this spot here: We will add 'gcc' command here to compile C sources
-
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I include/ -c -o ${ObjDIR}/main.o main.c
 
 
 # This links all your files. Remember that as you add *.o files, you need to
 # add them after start.o. If you don't add them at all, they won't be in your kernel!
+ld -T link.ld -o ${BinDIR}/kernel.bin ${ObjDIR}/start.o ${ObjDIR}/main.o
 echo "Done!"
